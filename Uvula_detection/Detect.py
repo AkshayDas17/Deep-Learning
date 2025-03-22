0dce7c54f1d6a13f4790f5ad8c0e71de53c9aa7a
Akshay <95156861+AkshayDas17@users.noreply.github.com>
2025-03-22 10:06:13 +0530

    Delete Uvula_detection directory

--- Uvula_detection/Detect.py
+++
@@ -1,3 +0,0 @@
-from ultralytics import YOLO
-model=YOLO('Uvula_detect.pt')
-model.predict(source="images (3).jpg",show=True,save=True,show_labels=True,show_conf=True,conf=0.5,save_txt=False,save_crop=False,line_width=2)
